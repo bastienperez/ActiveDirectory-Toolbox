775264e5bccb7a14ed13d6218a2ff9341045fbe0
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-08-05 14:53:19 +0200

    Update Create-LDAPLogsGPO.ps1

--- LDAP/Create-LDAPLogsGPO.ps1
+++ LDAP/Create-LDAPLogsGPO.ps1
@@ -1,8 +1,7 @@
 #requires -modules GroupPolicy
 #requires -modules ActiveDirectory
 
-# todo: disable user context
-# todo: security filtering : domain controllers
+# Tested on 2025-07-22-123543 on Windows Server 2022 on Azure : the domain controllers must be restarted to apply the GPO
 #Check if powershell is launched as elevevated
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
@@ -14,7 +13,7 @@ Import-Module ActiveDirectory
 Import-Module GroupPolicy
 
 function Create-LDAPLogsGPO {
-    Param(
+    param(
         [string]$GpoName = 'DC - Enable LDAP logs to level basic',
         [int]$DirectoryServiceLogMaxSize = '1048576000' # 524288000 = 512MB; 1048576000 = 1GB;2097152000 = 2GB
     )
@@ -22,23 +21,18 @@ function Create-LDAPLogsGPO {
     $DC = (Get-ADDomainController -Discover -Service ADWS).Name
 
     if ($DirectoryServiceLogMaxSize -lt 64000) {
-        Write-Warning "DirectoryServiceLogMaxSize parameter must be greater than or equal to 64Kb"
+        Write-Warning 'DirectoryServiceLogMaxSize parameter must be greater than or equal to 64Kb'
         return
     }
 
     if ($DirectoryServiceLogMaxSize % 64000 -ne 0) {
-        Write-Warning "DirectoryServiceLogMaxSize parameter must be a multiple of 64Kb"
+        Write-Warning 'DirectoryServiceLogMaxSize parameter must be a multiple of 64Kb'
         return
     }
   
     Write-Host -ForegroundColor Green "$GpoName - Create GPO"
     try {
-        $myGPO = New-GPO -Name $GpoName -Comment 'GPO to enable LDAP logs (level basic)' -Server $DC -ErrorAction Stop
-        
-        $dcOU = Get-ADOrganizationalUnit -Filter { Name -eq 'Domain Controllers' } 
-        
-        Write-Host -ForegroundColor Green "$GpoName - Link GPO to $($dcOU.DistinguishedName)"
-        $null = New-GPLink -Guid $myGPO.Id -Target $dcOU -LinkEnabled Yes -Server $DC -ErrorAction Stop
+        $gpoObject = New-GPO -Name $GpoName -Comment 'GPO to enable LDAP logs (level basic)' -Server $DC -ErrorAction Stop    
     }
     catch {
         Write-Warning $_.Exception.Message
@@ -64,17 +58,24 @@ function Create-LDAPLogsGPO {
     $domainSID = (Get-ADDomain).DomainSID.Value
 
     # Authenticated users has a well known SID : S-1-5-11. We need to get the group name to delete this group after the GPO creation
-    $authenticatedUsersGroupName = (New-Object System.Security.Principal.SecurityIdentifier("S-1-5-11")).Translate([System.Security.Principal.NTAccount]).Value.split('\')[1] 
+    $authenticatedUsersGroupName = (New-Object System.Security.Principal.SecurityIdentifier('S-1-5-11')).Translate([System.Security.Principal.NTAccount]).Value.split('\')[1] 
 
-    # Domain computers has a well known SID : S-1-5-domain-515. We need to get the group name to add this group after the GPO creation
-    $domainComputersGroupName = (New-Object System.Security.Principal.SecurityIdentifier("$domainSID-515")).Translate([System.Security.Principal.NTAccount]).Value.split('\')[1] 
+    # Domain Controllers group has a well known SID : S-1-5-domain-516. We need to get the group name to add this group after the GPO creation
+    $domainControllersGroupName = (New-Object System.Security.Principal.SecurityIdentifier("$domainSID-516")).Translate([System.Security.Principal.NTAccount]).Value.split('\')[1] 
 
-    Write-Host -ForegroundColor Cyan "$GpoName - Set Security Filtering to for domain computers only"
+    Write-Host -ForegroundColor Cyan "$GpoName - Remove Authenticated Users from GPO"
     $null = Set-GPPermission -Guid $gpoObject.Id -TargetName $authenticatedUsersGroupName -PermissionLevel None -TargetType Group -Confirm:$true
-    $null = Set-GPPermission -Guid $gpoObject.Id -TargetName $domainComputersGroupName -PermissionLevel GpoApply -TargetType Group -Confirm:$true
+    
+    Write-Host -ForegroundColor Cyan "$GpoName - Set Security Filtering to Domain Controllers group only"
+    $null = Set-GPPermission -Guid $gpoObject.Id -TargetName $domainControllersGroupName -PermissionLevel GpoApply -TargetType Group -Confirm:$true
 
-    $myGPO.GpoStatus = 'UserSettingsDisabled'
+    Write-Host -ForegroundColor Cyan "$GpoName - Disable User Settings"
+    $gpoObject.GpoStatus = 'UserSettingsDisabled'
 
+    $dcOU = Get-ADOrganizationalUnit -Filter { Name -eq 'Domain Controllers' } 
+        
+    Write-Host -ForegroundColor Green "$GpoName - Link GPO to $($dcOU.DistinguishedName)"
+    $null = New-GPLink -Guid $gpoObject.Id -Target $dcOU -LinkEnabled Yes -Server $DC -ErrorAction Stop
 }
 
 Create-LDAPLogsGPO
