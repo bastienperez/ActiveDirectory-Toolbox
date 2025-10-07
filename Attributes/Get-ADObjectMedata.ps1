b694b742c316f918766b1c62d85fae5d5dbcbcea
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-10-07 10:53:21 +0200

    Enhance Get-ADObjectMetadata script with detailed documentation and improved error handling; add support for searching objects using anr if DN is not found.

--- Attributes/Get-ADObjectMedata.ps1
+++ Attributes/Get-ADObjectMedata.ps1
@@ -1,14 +1,60 @@
-# Get-ADObjectMetadata 'DN' -Attributes ObjectClass,sn, cn
-# we can also use Get-ADReplicationAttributeMetadata 'xxxx' -Server xxx
-# TODO: test if recycle bin is enabled, otherwise LastTimeDeleted will 01/01/1601
+<#
+.SYNOPSIS
+    Get Active Directory Object Metadata
+
+.DESCRIPTION
+    This script retrieves metadata for a specified Active Directory object, including details about attribute changes.
+    It can filter the metadata based on specified attributes and can target a specific domain controller if needed.
+
+.PARAMETER ObjectDN
+    The distinguished name (DN) of the Active Directory object for which metadata is to be retrieved.
+
+.PARAMETER Attributes
+    An optional array of attribute names to filter the metadata results. If not specified, metadata for all attributes will be returned.
+
+.PARAMETER DomainController
+    An optional parameter to specify a particular domain controller to query. If not provided, the default domain controller will be used.
+
+.EXAMPLE
+    Get-ADObjectMetadata 'CN=John Doe,OU=Users,DC=example,DC=com'
+
+    Retrieves metadata for the specified user object.
+
+.EXAMPLE
+    Get-ADObjectMetadata 'CN=John Doe,OU=Users,DC=example,DC=com' -Attributes sn, cn
+
+    Retrieves metadata for the specified user object, filtering to show only the 'sn' and 'cn' attributes.
+
+.EXAMPLE
+    Get-ADObjectMetadata 'CN=John Doe,OU=Users,DC=example,DC=com' -DomainController 'dc01.example.com'
+
+    Retrieves metadata for the specified user object from the specified domain controller.
+
+.NOTES
+Author: Bastien Perez
+Date: 2025-10-07
+Version: 1.1.0
+
+.CHANGELOG
+[1.1.0] - 2025-10-07
+# Added
+- Add script parameters `-DomainController` to specify a domain controller.
+- Add possibility to search the object with anr (samaccountname, cn, etc.) if the DN is not found.
+
+[1.0.0] - 2023-xx-xx
+# Initial version  
+
+#>
 
 function Get-ADObjectMetadata {
     [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory = $true)]
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
         [String] $ObjectDN,
+        [Parameter(Mandatory = $false, Position = 1)]
+        [String[]] $Attributes,
         [Parameter(Mandatory = $false)]
-        [String[]] $Attributes
+        [String] $DomainController
     )
 
     try {
@@ -16,12 +62,43 @@ function Get-ADObjectMetadata {
     }
     catch {
         Write-Warning 'Unable to load ActiveDirectory module'
-        return
+        return 1
     }
 
+    if (-not $DomainController) {
+        $DomainController = (Get-ADDomainController -Discover -Service ADWS).HostName
+    }
+    
     [System.Collections.Generic.List[PSObject]] $objectMetadataArray = @()
 
-    $adObject = Get-ADObject $ObjectDN -Properties 'msDS-ReplAttributeMetaData'
+    try {
+        $adObject = Get-ADObject $ObjectDN -Properties 'msDS-ReplAttributeMetaData' -Server $DomainController -ErrorAction Stop
+    }
+    catch {
+        # https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-adts/1a9177f4-0272-4ab8-aa22-3c3eafd39e4b
+        Write-Warning "Unable to find object $ObjectDN (maybe you don't use a Distinguished Name (DN), the script will try to find it with anr=$ObjectDN)"
+        
+        try {
+            $adObject = Get-ADObject -LDAPFilter "(anr=$ObjectDN)" -Properties 'msDS-ReplAttributeMetaData' -Server $DomainController -ErrorAction Stop
+
+            if ( $adObject.Count -gt 1 ) {
+                Write-Warning "Multiple objects found with (anr=$ObjectDN), please use the exact Distinguished Name (DN) of the object"
+                return 1
+            }
+            elseif (-not $adObject) {
+                Write-Warning "No object found with (anr=$ObjectDN), please check the DN or the name of the object"
+                return 1
+            }
+            else {
+                Write-Host -ForegroundColor Cyan "Object found, DN: $($adObject.DistinguishedName)"
+            }
+        }
+        catch {
+            Write-Warning "Unable to find object $ObjectDN with Get-ADObject -LDAPFilter (anr=$ObjectDN), please check the DN or the name of the object"
+            return 1
+        }
+    }
+
     $replAttributeMetaData = $adObject.'msDS-ReplAttributeMetaData'
     $replAttributeMetaData = '<root>' + $ReplAttributeMetaData + '</root>'
     $replAttributeMetaData = $ReplAttributeMetaData.Replace([char]0, ' ')
